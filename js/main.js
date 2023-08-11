99b69d3437e9e70a53f699efef835e1951bdaa9b
faridrabahi <1185773@cegepoutaouais.qc.ca>
2023-08-11 00:29:57 -0400

    add event listner and handler for advanced functions

--- js/main.js
+++ js/main.js
@@ -3,34 +3,26 @@
 
 // Import the provided advanced functions
 import {
-  sin,
-  cos,
-  tan,
-  pi,
-  percentage,
-  square,
-  cube,
-  nthRoot,
-  binaryFactorial,
-  naturalLog,
-  commonLog,
+  advancedOperation
 } from "./helpers.js"; // Adjust the import path
 
 // Create an instance of the Calculator class
  const calculator = new Calculator();
 
-const btnNumbers = document.querySelectorAll('.calculator__number')
-const btnOperations = document.querySelectorAll('.calculator__operator')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousInput = document.querySelector('[data-previous-input]')
+const btnNumbers = document.querySelectorAll('.calculator__number');
+const btnOperations = document.querySelectorAll('.calculator__operator');
+const btnAdvancedOperations = document.querySelectorAll('.calculator__function');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousInput = document.querySelector('[data-previous-input]');
 const currentInput = document.querySelector('[data-current-input]');
-const btnFactorial = document.querySelector('[data-fact]');
 
 let prevOperand = previousInput.innerText;
 let currentOperand = currentInput.innerText;
 let operation;
+let advancedOperator;
+
 
 // Events handlers
 
@@ -39,6 +31,7 @@ const reset = () => {
   prevOperand = "";
   currentOperand = "";
   operation = undefined;
+  advancedOperator = undefined;
 };
 
 //delete an input
@@ -63,7 +56,15 @@ const operationSelection = (operate) => {
   currentOperand = "";
 };
 
-//function to implement calculation
+// function to select an advanced operation
+const advancedOperationSelection = (operate) => {
+  if (currentInput === "") return;
+  advancedOperator = operate.toString();
+  prevOperand = currentOperand;
+  currentOperand = "";
+};
+
+//function to implement basic calculation (addition, division ...) 
 const compute =(operate)=> {
   let result;
   let prev = parseFloat(prevOperand);
@@ -75,6 +76,8 @@ const compute =(operate)=> {
   prevOperand = "";
 };
 
+
+// Function to handle degit number
 const displayNumber=(number)=> {
   const stringNumber = number.toString()
   const integerDigits = parseFloat(stringNumber.split('.')[0])
@@ -92,22 +95,35 @@ const displayNumber=(number)=> {
   }
 }
 
+// Function to update the result afetr each operation
 const updateDisplay=()=> {
   currentInput.innerText =
-    displayNumber(currentOperand)
+    displayNumber(currentOperand);
+   
   if (operation != null) {
-    previousInput.innerText =
-      `${displayNumber(prevOperand)} ${operation}`
+     previousInput.innerText =
+      `${displayNumber(prevOperand)} ${operation}`;
   } else {
     previousInput.innerText = ''
   }
 }
 
+// Function to handle advanced operations like(sin, cos ...)
+function handleAdvancedOperation(advancedOperator) {
+  let result;
+  let prev = parseFloat(prevOperand);
+  let current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return
+  result=advancedOperation(advancedOperator,prev,current);
+  previousInput.textContent = `${advancedOperator}(${prev},${current})`;
+  currentInput.textContent = result; 
+}
+
+
 //Events listners 
 
 btnNumbers.forEach(button => {
   button.addEventListener('click', () => {
-    console.log(button.innerText);
     addNumber(button.innerText);
     updateDisplay();
   })
@@ -115,16 +131,19 @@ btnNumbers.forEach(button => {
 
 btnOperations.forEach(button => {
   button.addEventListener('click', () => {
-    console.log(button.innerText);
     operationSelection(button.innerText);
     updateDisplay();
   })
 })
 
 equalsButton.addEventListener('click', button => {
-  console.log(button.innerText);
-  compute(operation);
-  updateDisplay();
+  if (currentInput === "") return;
+  if (operation){
+    compute(operation);
+    updateDisplay();
+  } else if (advancedOperator) {
+    handleAdvancedOperation(advancedOperator,prevOperand,currentOperand);
+  }
 })
 
 allClearButton.addEventListener('click', button => {
@@ -137,7 +156,39 @@ deleteButton.addEventListener('click', button => {
   updateDisplay();
 })
 
-btnFactorial.addEventListener('click', button => {
-  currentOperand =binaryFactorial(parseFloat(currentOperand));
+btnAdvancedOperations.forEach( button => {
+  button.addEventListener('click', () => {
+    advancedOperationSelection(button.innerText);
+    updateDisplay();
+  })
+});
+
+// add event listener for keyboard support
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+console.log(key);
+  switch (key) {
+    case "Backspace":
+      deleteInput();
+      break;
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      operationSelection(key);
+      break;
+    case "Enter":
+      compute();
+      break;
+    case "Escape":
+      reset();
+      break;
+    default:
+      if (!isNaN(key) || key === ".") {
+        addNumber(key);
+      }
+      break;
+  }
+
   updateDisplay();
-})
+});
